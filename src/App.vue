43cf3879e28fda80c9d322c3b637b3f9a2f01ab6
Gjermund Bjaanes <gjermund@empower.eco>
2022-03-18 09:37:57 +0100

    Added descriptions for algos

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@
             </el-form-item>
             <el-form-item>
               <el-button type="primary" @click="submitForm(formRef)"
-                >Submit</el-button
+                >Check estimated prices</el-button
               >
             </el-form-item>
           </el-form>
@@ -42,25 +42,80 @@
             size="large"
             border
           >
-            <el-descriptions-item label="Fren id">{{
-              fren.id
-            }}</el-descriptions-item>
-            <el-descriptions-item label="Rarity score">{{
-              Math.round(fren.rarity_score)
-            }}</el-descriptions-item>
-            <el-descriptions-item label="KRV"
-              >{{ Math.round(fren.krv) }} Stars</el-descriptions-item
-            >
-            <el-descriptions-item label="KARV"
-              >{{ Math.round(fren.karv_present) }} Stars (today) -
-              {{ Math.round(fren.karv_future) }} Stars (2 years from
-              now)</el-descriptions-item
-            >
-            <el-descriptions-item label="KHRV">{{
-              Math.round(fren.khrv)
-            }}</el-descriptions-item>
-            <el-descriptions-item label="KHARV"
-              >{{ Math.round(fren.kharv_present) }} Stars (today) -
+            <el-descriptions-item label="Fren id">
+              {{ fren.id }}
+            </el-descriptions-item>
+            <el-descriptions-item label="Rarity score">
+              {{ Math.round(fren.rarity_score) }}
+            </el-descriptions-item>
+            <el-descriptions-item label="KRV">
+              <template v-slot:label>
+                <el-popover
+                  placement="top-start"
+                  title="KRV"
+                  :width="500"
+                  trigger="hover"
+                >
+                  <template #reference>
+                    KRV ℹ️
+                  </template>
+                  The Kryoten Rarity Valuation. <br>
+                  This is an algorithm designed by Kryoten to come up with a PRE-Market Valuation based on solely rarity score. This is the simplest metric and as valued by Kryoten, should be the lowest expected price to trade a Fren for given there are no official market values or floor prices yet.
+                </el-popover>
+              </template>
+              {{ Math.round(fren.krv) }} Stars
+            </el-descriptions-item>
+            <el-descriptions-item label="KARV">
+              <template v-slot:label>
+                <el-popover
+                  placement="top-start"
+                  title="KARV"
+                  :width="500"
+                  trigger="hover"
+                >
+                  <template #reference>
+                    KARV ℹ️
+                  </template>
+                  The Kryoten Accelerated Rarity Valuation.<br>
+                  This is the companion algorithm to give a more speculative price based on other NFT projects, also accounting for value and growth in floor prices, with similar supply. This number is a range speculating todays KARV, and a future price based on a time span of 2 years.
+                </el-popover>
+              </template>
+              {{ Math.round(fren.karv_present) }} Stars (today) -
+              {{ Math.round(fren.karv_future) }} Stars (2 years from now)
+            </el-descriptions-item>
+            <el-descriptions-item label="KHRV">
+              <template v-slot:label>
+                <el-popover
+                  placement="top-start"
+                  title="KHRV"
+                  :width="500"
+                  trigger="hover"
+                >
+                  <template #reference>
+                    KHRV ℹ️
+                  </template>
+                  The Kryoten Hubble Rarity Valuation.<br>
+                  This is a similar algorithm to KRV, however this is accounting for rarity scores given by hubble.tools, given they have a different scoring system. This dilutes the KRV rating significantly and more so is a companion for the KHARV algorithm.
+                </el-popover>
+              </template>
+              {{ Math.round(fren.khrv) }}
+            </el-descriptions-item>
+            <el-descriptions-item label="KHARV">
+              <template v-slot:label>
+                <el-popover
+                  placement="top-start"
+                  title="KHARV"
+                  :width="500"
+                  trigger="hover"
+                >
+                  <template #reference>
+                    KHARV ℹ️
+                  </template>
+                  The Kryoten Hubble Accelerated Valuation.<br>
+                  This algorithm has had all factors up to this point accounted for and could be the most accurate for fair PRE-Market valuations. Similar to KARV, is also a range speculating todays KHARV, and a future price based on a time span of 2 years.
+                </el-popover>
+              </template>
+              {{ Math.round(fren.kharv_present) }} Stars (today) -
               {{ Math.round(fren.kharv_future) }} Stars (2 years from
               now)</el-descriptions-item
             >
@@ -80,6 +135,7 @@
 <script setup lang="ts">
 import { reactive, ref } from "vue";
 import type { FormInstance } from "element-plus";
+import { InfoFilled } from "@element-plus/icons-vue";
 import nftRarity from "@/assets/values.json"; // TODO: transform into map instead of array for easier access
 
 const formRef = ref<FormInstance>();
